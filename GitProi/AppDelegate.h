29b8d0ac21a92b76c1a07b78cdc9cc3453bb7abb
SPRUCE <spruce@Administrators-MacBook-Pro.local>
2015-05-14 18:52:20 +0530

    Initial Commit

---
+++ GitProi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitProi
+//
+//  Created by SPRUCE on 14/05/15.
+//  Copyright (c) 2015 Perpetro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitProi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitProi
+//
+//  Created by SPRUCE on 14/05/15.
+//  Copyright (c) 2015 Perpetro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
